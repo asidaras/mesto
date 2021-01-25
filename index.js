205d89c06a154b11c19764844bfc022bf7567108
Антон Сидарас <sian111@yandex.ru>
2021-01-25 16:07:14 +0700

    mesto_v-0.4
    Добавлен Попап

---
+++ index.js
@@ -0,0 +1,6 @@
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+
+function openPopup(){
+  let popup = document.querySelector('.popup');
+}
