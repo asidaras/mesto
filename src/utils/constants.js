77ac802b3c7c2e44f603175565fbfdc16cdbe733
Антон Сидарас <sian111@yandex.ru>
2021-04-13 16:22:07 +0700

    gh-clean

--- src/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const profile = document.querySelector(".profile");
-export const profileEditButton = profile.querySelector(".profile__edit-button");
-export const profileAddButton = profile.querySelector(".profile__add-button");
-export const profileAvatarButton = profile.querySelector(".profile__avatar-button");
